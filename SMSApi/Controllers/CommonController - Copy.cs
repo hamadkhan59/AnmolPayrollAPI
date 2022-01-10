82c04d3b4d873b5d2dea1692f73788fc65e01ac1
hamadkhan59 <86347085+hamadkhan59@users.noreply.github.com>
2022-01-10 11:12:35 +0500

    Further changes

--- SMSApi/Controllers/CommonController - Copy.cs
+++ SMSApi/Controllers/CommonController - Copy.cs
@@ -55,9 +55,9 @@ namespace SMSApi.Controllers
 
         DateTime GetDate(string attendanceDate)
         {
-            string[] attendanceData = attendanceDate.Split(' ');
-            string[] date = attendanceData[0].Split('/');
-            string[] time = attendanceData[1].Split(':');
+            string [] attendanceData = attendanceDate.Split(' ');
+            string [] date = attendanceData[0].Split('/');
+            string [] time = attendanceData[1].Split(':');
             string am_pm = attendanceData[2];
             //string newDate = date[1] + "/" + date[0] + "/" + date[2];
             int year = int.Parse(date[2]);
@@ -72,7 +72,7 @@ namespace SMSApi.Controllers
             //string dateTime = newDate + " " + time + " " + am_pm;
             if (am_pm.ToLower().Contains("pm"))
             {
-                if (hours != 12)
+                if(hours != 12)
                     hours += 12;
             }
             else if (am_pm.ToLower().Contains("am"))
@@ -104,11 +104,13 @@ namespace SMSApi.Controllers
             DataSet dataSet = (new DAL_Staff_Reports()).GetNewStaffAttendanceLogs();
 
             var table = dataSet.Tables[0];
+            int count = 0;
             foreach (DataRow row in table.Rows)
             {
                 string staffId = row[1].ToString();
                 string attendanceDate = row[3].ToString();
                 AddStaffAttendance(staffId, attendanceDate);
+                count++;
             }
 
             return "SUCCESS";
@@ -141,52 +143,102 @@ namespace SMSApi.Controllers
                     int logId = staffRepo.GetAttendanceLogId(int.Parse(staffId), attendanceDate);
                     //int logId = 0;
 
-                    if (logId == 0)
+                    if (logId == 0 || true)
                     {
-                        StaffAttendanceLog log = new StaffAttendanceLog();
-                        log.DateTime = attendanceTime;
-                        log.DateTimeString = attendanceDate;
-                        log.StaffId = int.Parse(staffId);
-                        staffRepo.AddStaffAttendnaceLogs(log);
+                        //StaffAttendanceLog log = new StaffAttendanceLog();
+                        //log.DateTime = attendanceTime;
+                        //log.DateTimeString = attendanceDate;
+                        //log.StaffId = int.Parse(staffId);
+                        //staffRepo.AddStaffAttendnaceLogs(log);
 
                         StaffAttandance attObj = staffRepo.GetStaffDailyAttendance(attendanceTime, 0);
-                        if (attObj == null)
-                        {
-                            LogWriter.WriteLog("Attendance is not found for the date : " + attendanceTime.Date + ", staff Id : " + staffId);
-                            AttendanceSettings.TodaysAttendance = true;
 
-                            List<Staff> staffList = staffRepo.GetAllStaff();
-                            foreach (Staff staff in staffList)
+                        //if (attObj == null)
+                        //{
+                        //    LogWriter.WriteLog("Attendance is not found for the date : " + attendanceTime.Date + ", staff Id : " + staffId);
+                        //    AttendanceSettings.TodaysAttendance = true;
+
+                        //    List<Staff> staffList = staffRepo.GetAllStaff();
+                        //    foreach (Staff staff in staffList)
+                        //    {
+                        //        StaffAttandance tempObj = new StaffAttandance();
+                        //        tempObj.StaffId = staff.StaffId;
+                        //        tempObj.Date = attendanceTime.Date;
+                        //        tempObj.CreatedOn = DateTime.Now;
+                        //        tempObj.Time = AttendanceSettings.TimeIn;
+                        //        tempObj.OutTime = AttendanceSettings.TimeIn;
+                        //        tempObj.Status = 2;
+
+                        //        int attendance = staffRepo.AddStaffAttendnace(tempObj);
+                        //    }
+                        //}
+                        attObj = staffRepo.SearchStaffDailyAttendance(attendanceTime, int.Parse(staffId));
+
+                        if (attObj == null || attObj.Id > 67052)
+                        {
+                            if (attObj == null)
                             {
                                 StaffAttandance tempObj = new StaffAttandance();
-                                tempObj.StaffId = staff.StaffId;
+                                tempObj.StaffId = int.Parse(staffId);
                                 tempObj.Date = attendanceTime.Date;
                                 tempObj.CreatedOn = DateTime.Now;
                                 tempObj.Time = AttendanceSettings.TimeIn;
                                 tempObj.OutTime = AttendanceSettings.TimeIn;
-                                tempObj.Status = 2;
+                                tempObj.Status = 1;
 
                                 int attendance = staffRepo.AddStaffAttendnace(tempObj);
+                                attObj = tempObj;
                             }
-                        }
-                        attObj = staffRepo.SearchStaffDailyAttendance(attendanceTime, int.Parse(staffId));
 
-                        string time = attendanceTime.Hour.ToString().PadLeft(2, '0') + ":" + attendanceTime.Minute.ToString().PadLeft(2, '0');
-                        var attDetail = staffRepo.GetTopStaffAttendanceDetailByAttId(attObj.Id);
-                        string type = AttendanceSettings.TIME_IN;
-                        if (attDetail == null || attDetail.TimeOut != null)
-                        {
-                            attDetail = new StaffAttendanceDetail();
-                            attDetail.AttendanceId = attObj.Id;
-                            attDetail.TimeIn = time;
-                            attDetail.TimeInString = attendanceDate;
-                            staffRepo.AddStaffAttendnaceDetail(attDetail);
-                        }
-                        else
-                        {
-                            if (attDetail.TimeOut == null)
+                            LogWriter.WriteLog("Adding attendance");
+
+
+                            string time = attendanceTime.Hour.ToString().PadLeft(2, '0') + ":" + attendanceTime.Minute.ToString().PadLeft(2, '0');
+                            var attDetail = staffRepo.GetTopStaffAttendanceDetailByAttId(attObj.Id);
+                            string type = AttendanceSettings.TIME_IN;
+                            if (attDetail == null || attDetail.TimeOut != null)
                             {
-                                type = AttendanceSettings.TIME_OUT;
+                                LogWriter.WriteLog("Adding initial attendance detail");
+                                attDetail = new StaffAttendanceDetail();
+                                attDetail.AttendanceId = attObj.Id;
+                                attDetail.TimeIn = time;
+                                attDetail.TimeInString = attendanceDate;
+                                staffRepo.AddStaffAttendnaceDetail(attDetail);
+                            }
+                            else
+                            {
+                                LogWriter.WriteLog("Adding next attendance detail");
+                                if (attDetail.TimeOut == null)
+                                {
+                                    type = AttendanceSettings.TIME_OUT;
+                                    int hours = int.Parse(time.Split(':')[0]);
+                                    int mins = int.Parse(time.Split(':')[1]);
+                                    if (hours >= 19 || hours <= 2)
+                                    {
+                                        if (mins >= 40)
+                                            time = time.Split(':')[0] + ":" + mins;
+                                        else
+                                            time = time.Split(':')[0] + ":00";
+                                    }
+
+                                    attDetail.TimeOut = time;
+                                    attDetail.TimeOutString = attendanceDate;
+                                    staffRepo.UpdateStaffAttendanceDetail(attDetail);
+                                }
+                            }
+
+                            attObj.Status = 1;
+                            LogWriter.WriteLog("updating attendance status");
+                            if (type == AttendanceSettings.TIME_IN)
+                            {
+                                if (attObj.Time == AttendanceSettings.TimeIn)
+                                {
+                                    attObj.Time = time;
+                                    error = AttendanceSettings.TIME_IN;
+                                }
+                            }
+                            else
+                            {
                                 int hours = int.Parse(time.Split(':')[0]);
                                 int mins = int.Parse(time.Split(':')[1]);
                                 if (hours >= 19 || hours <= 2)
@@ -196,37 +248,11 @@ namespace SMSApi.Controllers
                                     else
                                         time = time.Split(':')[0] + ":00";
                                 }
-
-                                attDetail.TimeOut = time;
-                                attDetail.TimeOutString = attendanceDate;
-                                staffRepo.UpdateStaffAttendanceDetail(attDetail);
+                                attObj.OutTime = time;
+                                error = AttendanceSettings.TIME_OUT;
                             }
+                            staffRepo.UpdateStaffAttendance(attObj);
                         }
-
-                        attObj.Status = 1;
-                        if (type == AttendanceSettings.TIME_IN)
-                        {
-                            if (attObj.Time == AttendanceSettings.TimeIn)
-                            {
-                                attObj.Time = time;
-                                error = AttendanceSettings.TIME_IN;
-                            }
-                        }
-                        else
-                        {
-                            int hours = int.Parse(time.Split(':')[0]);
-                            int mins = int.Parse(time.Split(':')[1]);
-                            if (hours >= 19 || hours <= 2)
-                            {
-                                if (mins >= 40)
-                                    time = time.Split(':')[0] + ":" + mins;
-                                else
-                                    time = time.Split(':')[0] + ":00";
-                            }
-                            attObj.OutTime = time;
-                            error = AttendanceSettings.TIME_OUT;
-                        }
-                        staffRepo.UpdateStaffAttendance(attObj);
                     }
                 }
             }
@@ -237,6 +263,8 @@ namespace SMSApi.Controllers
                 error += "\n===================================\n";
                 error += ex.StackTrace.ToString();
                 error += "\n===================================\n";
+                LogWriter.WriteLog(error);
+
             }
             return error;
         }
@@ -283,7 +311,7 @@ namespace SMSApi.Controllers
                 proc.StartInfo = new ProcessStartInfo(exePath);
                 proc.StartInfo.WorkingDirectory = directory;
                 proc.StartInfo.UseShellExecute = true;
-
+                
 
                 proc.Start();
 
